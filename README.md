5a774011287bff8f088ce8cf955a7a66e05dcdc7
uwadonat <uwadonat@gmail.com>
2022-08-15 12:29:59 +0200

    Add subject

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# javascript_course
+# JavaScript course
+
+This application has target of helping junior JavaScript developer.We will be sharing challenges solution, any issue you may face while writing a javascript, feel free to share it here will be more than grad to assist. 
+
+## Built With
+- HTML
+- CSS
+- JavaScript
+- Git
+- GitHub
+- VSCode
+
+
+## Getting Started
+
+To get a local copy up and running follow these simple example steps.
+
+### Prerequisites
+
+- A source-code editor (preferably Visual Studio Code)
+
+
+### Using it Locally
+
+- Clone the project
+
+git clone https://github.com/uwadonat/javascript_course
+
+cd javascript_course
+
+
+## Authors
+
+👤 **Donat Uwamahoro**
+
+- GitHub: [uwadonat](https://github.com/uwadonat)
+- LinkedIn: [uwadonat](https://www.linkedin.com/in/melaku-eshetu/)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](../../issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Hat tip to anyone whose code was used
+- Microverse program
+- My standup team
+- Microverse team
+- My family's support
+
+## 📝 License
+
+This project is open source project.
--- script.js
+++ script.js
@@ -1,39 +1,48 @@
-// function displaySection (event) {
-//     let clicked = event.target.event
-//     console.log(event);
-//  document.getElementById("clicked").style.display="block";
-// }
 
 function openHeading(evt, headingName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
-  
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-  
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-  
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(headingName).style.display = "block";
-    evt.currentTarget.className += "active";
-  } 
+  // Declare all variables
+  var i, tabcontent, tablinks;
 
-  // define an object to save challenges, description and solution
-
-  let challenges = [{ Name: "Binary", Descrition: "Binary number", Sulution: "console.log('1111111')"}];
-
- for (let i=0; i<challenges.length;i++)  {
-
-    document.getElementById("title").innerHTML = challenges[i].Name;
-   
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
 
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
 
- 
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(headingName).style.display = "block";
+  evt.currentTarget.className += "active";
+}
+
+// define an object to save Topics, description and solution
+
+let subjects = [
+  {
+    Name: "Kwizera Imana",
+    Description: "Kwizera Imana bisobanura iki ?",
+    Solution: "Ni iyihe migisha Imana yadusezeranyije? Ni iki kikwemeza ko ibyo Ijambo ry’Imana rivuga ari ukuri? Mu bice bikurikira turasuzuma bimwe mu bintu Imana yadusezeranyije n’impamvu twizera ko izabiduha. Nanone turareba icyo twakora kugira ngo tuzabibone.",
+  },
+  {
+    Name: "Umugisha uva ku Mana",
+    Description: "Wakora iki ngo Imana iguhe imigisha?",
+    Solution: "Ni iyihe migisha Imana yadusezeranyije? Ni iki kikwemeza ko ibyo Ijambo ry’Imana rivuga ari ukuri? Mu bice bikurikira turasuzuma bimwe mu bintu Imana yadusezeranyije n’impamvu twizera ko izabiduha. Nanone turareba icyo twakora kugira ngo tuzabibone",
+  },
+];
+
+for (let i = 0; i < subjects.length; i++) {
+  document.getElementById(
+    "title"
+  ).innerHTML += `<div>
+  <h3 id="${i}">${subjects[i].Name}</h3>
+  <p> ${subjects[i].Description} </p>
+  <div> ${subjects[i].Solution} </div>
+  <hr>
+  </div>`;
+}
+
